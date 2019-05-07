409f6b3142aeec406c198d25531d8e4212c31793
Mohd Haider <mohdhaider30@gmail.com>
2019-05-07 20:49:27 +0530

    Creating User and Superuser and it's test cases Added

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,25 @@
+# Using alpine Docker image
 FROM python:3.7-alpine
 
 # MAINTAINER is deprecated so use LABEL instead
 LABEL Mohd Haider Ali 
 
+# Using unbuffered python to optimise python execution
 ENV PYTHONUNBUFFERED 1
 
+# Copying req. file from local to the Container
 COPY ./requirements.txt /requirements.txt
+
+# Installing req in Container
 RUN pip install -r /requirements.txt
 
+# Creating app dir and copying all local app data to Container
 RUN mkdir /app
 WORKDIR /app
 COPY ./app /app
 
+# Adding user in Container to use the App so as to secure it from 
+# Attackers
 RUN adduser -D user
 USER user
 
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_user_created_with_email_is_successful(self):
+        """This function checks if user is created with given email"""
+        email = "mohdhaider30@gmail.com"
+        password = "Testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "mohdhaider30@GMAIL.com"
+        user = get_user_model().objects.create_user(email=email)
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, password="Test")
+
+    def test_create_superuser(self):
+        """Test creating new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'mohdhaider30@gmail.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
