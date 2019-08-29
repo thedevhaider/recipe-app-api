52297d61404f40d8954d431625f477ebcf38708c
Mohd Haider Ali <haider.ali@Mascarenhas-2.local>
2019-08-29 19:16:21 +0530

    Added tests and endpoint for Managing user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 CREATE_USER_API = reverse('user:create')
 TOKEN_URI = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -14,6 +15,7 @@ def create_user(**params):
 
 
 class PublicUserApiTests(TestCase):
+    """Test APIs that don't require authentication"""
 
     def setUp(self):
         self.client = APIClient()
@@ -108,3 +110,52 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URI, payload)
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unautherized(self):
+        """Test that authentication is required for users"""
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test APIs that requires authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@gmail.com',
+            password='testpass123',
+            name='testname'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retreiving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'email': self.user.email,
+            'name': self.user.name
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that post is not allowed on me url"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_profile_success(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {
+            'email': 'newtest@gmail.com',
+            'password': 'newpassword'
+        }
+        res = self.client.patch(ME_URL, payload)
+
+        # Refresh the user object with latest values from db
+        self.user.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user.email, payload['email'])
+        self.assertTrue(self.user.check_password(payload['password']))
