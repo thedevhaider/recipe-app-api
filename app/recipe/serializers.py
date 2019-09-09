b5786898be072d3b6df0c44dd95d2c8b30956f6d
Mohd Haider Ali <haider.ali@Mascarenhas-2.local>
2019-09-09 20:54:02 +0530

    Added Filtering Recipes by Tags and Ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -36,6 +36,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = (
             'id', 'title', 'ingredients', 'tags', 'time_minutes', 'price',
+            'image',
             'link',
         )
         read_only_fields = ('id',)
